f99b693442a5742089799ed10ba40145e07ab636
segural <luciano.g.segura@gmail.com>
2022-05-27 12:40:59 -0300

    lo pase a express

--- app.js
+++ app.js
@@ -1,52 +1,21 @@
-let valorTicket = 200;
-let alertEl = document.getElementById("alert-el")
-let alertDiv = document.getElementById("alertcont")
-document.getElementById("inputcantidad").value = 1
+//requiero paquetes y genero las variables(espress,path)
+const express = require ('express');
+const path = require ('path');
+const app = express ();
+const publicPath = path.resolve ('public');
+// const bodyParser = require ('bodyParser');
 
-function resumen() {
-    let cantidadEl = Math.ceil(document.getElementById("inputcantidad").value)
-    let categoriaEl = document.getElementById("categoria-el").value
-    document.getElementById("inputcantidad").value = cantidadEl 
+app.use(express.static (publicPath));
 
-    if (cantidadEl < 0) {
-        cantidadEl = 1
-        document.getElementById("inputcantidad").value = cantidadEl 
-    } else if (cantidadEl > 10) {
-        cantidadEl = 10
-        document.getElementById("inputcantidad").value = cantidadEl 
-        var tag = document.createElement("div")
-        tag.classList.add('alert-danger')
-        tag.classList.add('alert')
-        let text = document.createTextNode("Cantidad máxima de entradas por persona: 10");
-        tag.appendChild(text);
-        alertDiv.appendChild(tag);
-    }
+// app.use(bodyParser.urlencoded({ extended: true })); 
 
-    if (categoriaEl == "estudiante") {
-        let totalAPagar = valorTicket * cantidadEl
-        totalAPagar = totalAPagar * 0.20
-        alertEl.innerText = "Total a Pagar: $"
-        alertEl.innerText = alertEl.innerText + " " + totalAPagar
-    } else if (categoriaEl == "trainee") {
-        let totalAPagar = valorTicket * cantidadEl
-        totalAPagar = totalAPagar * 0.50
-        alertEl.innerText = "Total a Pagar: $"
-        alertEl.innerText = alertEl.innerText + " " + totalAPagar
-    } else if (categoriaEl == "junior") {
-        let totalAPagar = valorTicket * cantidadEl
-        totalAPagar = totalAPagar * 0.85
-        alertEl.innerText = "Total a Pagar: $"
-        alertEl.innerText = alertEl.innerText + " " + totalAPagar
-    } else {
-        let totalAPagar = valorTicket * cantidadEl
-        alertEl.innerText = "Total a Pagar: $"
-        alertEl.innerText = alertEl.innerText + " " + totalAPagar
-    }
-}
 
-function borrar() {
-    let cantidadEl = Math.ceil(document.getElementById("inputcantidad").value)
-    let categoriaEl = document.getElementById("categoria-el").value
-    document.getElementById("inputcantidad").value = 1
-    alertEl.innerText = "Total a Pagar: $"
-}
+//hago la peticion al server de las htmls
+app.get ('/', (req,res) =>{
+    res.sendFile (path.resolve ('./index.html'))
+});
+
+//levanto server express en puerto 3000
+app.listen (process.env.PORT || 3000, function () {
+    console.log ('Servidor corriendo en el puerto 3000');
+});
